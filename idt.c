e7547a222c13dbe8457d8543a960eee1751f844e
AnnieRay97 <annieray97@gmail.>
2015-10-10 11:43:14 +0530

    Create idt.c

---
+++ idt.c
@@ -0,0 +1,51 @@
+/* Defines an IDT entry*/
+
+struct idt_entry
+{
+	unsigned short base_lo;
+	unsigned short sel;
+	unsigned char always0;
+	unsigned char flags;
+	unsigned short base_hi;
+}__attribute__((packed));
+
+struct idt_ptr
+{
+	unsigned short limit;
+	unsigned int base;
+}__attribute__((packed));
+
+/*Declare an IDT of 256 entries. Although we will use only the first 32 entries , the rest exists as a bit of a trap. If any undefined IDT entry is hit, it normally will cause an "Unhandled Interrupt" exception. Any Descriptor for which the 'presence' bit is cleared (0) will generate an "Unhandled Interrupt" exception*/
+
+struct idt_entry idt[256];
+struct idt_ptr idtp;
+
+/*This exists in start.asm and is used to load our IDT*/
+
+extern void idt_load()__asm__("idt_load");
+
+void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags)
+{
+	idt[num].base_lo = (base & 0xFFFF);
+	idt[num].sel = sel;
+	idt[num].always0 = 0;
+	idt[num].flags = flags;
+	idt[num].base_hi = (base >> 16)&0xFFFF;
+}
+
+/* Installs the IDT*/
+void idt_install()
+{
+	/* Sets the special IDT pointer up, just like in 'gdt.c'*/
+	idtp.limit = (sizeof(struct idt_entry)*256) - 1;
+	idtp.base = &idt;
+
+	/* Clear out the entire IDT, initializing it to zeroes*/
+	memset(&idt, 0, sizeof(struct idt_entry)*256);
+
+	/* Add any new ISRs to the IDT here using idt_set_gate*/
+
+	/* Points the processor's internal register to the new IDT*/
+
+	idt_load();
+}
